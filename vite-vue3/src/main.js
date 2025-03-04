5b6e6969d8773ca8b9310e97645bc73a77b761e1
boboZh <18201491619@163.com>
2025-03-04 23:53:13 +0800

    feat: glob api

--- vite-vue3/src/main.js
+++ vite-vue3/src/main.js
@@ -8,4 +8,16 @@ import App from './App'
 // worker.onmessage = function (e) {
 //   console.log(e);
 // };
+
+// glob功能： 基于fast-glob
+
+const globalModules = import.meta.glob('./glob/*-[0-9].js') // 名字可以使用正则表达式
+const globalEagers = import.meta.glob('./glob/*', {
+  eager: true,
+})
+console.log('globalEagers: ', globalEagers)
+Object.entries(globalModules).forEach(([key, value]) => {
+  value().then((module) => console.log(key + ':' + module.default))
+})
+
 createApp(App).mount('#app')
--- vite-vue3/vite.config.js
+++ vite-vue3/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsxPlugin from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsxPlugin()],
   resolve: {
     alias: {
-      "@styles": "/src/styles",
+      '@styles': '/src/styles',
     },
   },
-});
+  build: {
+    manifest: true,
+  },
+})
