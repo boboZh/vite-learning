5ed7b173a92dc6e7ad7bcdcb84b9f31ede30b99a
boboZh <18201491619@163.com>
2025-03-04 23:53:25 +0800

    feat: 预编译

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { throttle } from "lodash-es";
 
-createRoot(document.getElementById('root')).render(
+console.log(throttle);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    // 指定哪些需要预编译，哪些不需要,vite默认会扫描一遍依赖不需要手动指定。如果开发过程中遇到了类似的the requested module 'xx/xx/xx/xx.js' does not provide an export named 'default'这种情况，可以尝试把这个包加在include里，然后重新运行一下。某些情况下，我们使用的依赖又依赖于别的第三方的依赖，这个时候vite扫描不到才会出现。
+    exclude: [], //
+  },
+});
